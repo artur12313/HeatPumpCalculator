cf136ea18737abda98d24697d76aaa4239bdcb4c
Artur Pisula <arturpisula11@wp.pl>
2022-07-12 00:24:14 +0200

    Added new calc operations

--- resources/views/calculator.blade.php
+++ resources/views/calculator.blade.php
@@ -43,11 +43,29 @@
           <x-jet-input id="amount" class="block mt-1 w-full" type="number" name="amount" required autofocus onchange="avgInstallment()"/>
         </div>
       </div>
-      <div class="footer-form mt-4 flex justify-end">
+      <div class="footer-form mt-4 flex justify-between">
+      <button type="button" class="bg-green-600/75 hover:bg-green-600/50 text-white py-1 px-4 text-lg" id="previewTrigger" onclick="showPreview()">Podgląd</button>
         <button type="submit" class="bg-sky-600/75 hover:bg-sky-600/50 text-white py-1 px-4 text-lg">Zapisz</button>
     </div>
     </form>
+    <div id="preview">
+    </div>
     <script>
+      function showPreview()
+      {
+        var content = document.getElementById('preview');
+        content.style.display = "block";
+
+        var previewTrigger = document.getElementById('previewTrigger').addEventListener("click", function () {
+        if(content.style.display == 'none')
+        {
+            content.style.display = "block";
+        }else{
+            content.style.display = "none";
+        }
+        });
+      }
+
       function MonthlyInterest()
       {
         let interest = Number(document.getElementById('interest').value == "" ? 0 : document.getElementById('interest').value);
@@ -60,34 +78,39 @@
 
       function avgInstallment()
       {
+        var i = 1;
         let oprocentowanie = Number(document.getElementById('interest').value == "" ? 0 : document.getElementById('interest').value) / 100;
         let liczba_rat = Number(document.getElementById('terms').value == "" ? 0 : document.getElementById('terms').value);
         let kwota_kredytu = Number(document.getElementById('amount').value == "" ? 0 : document.getElementById('amount').value);
 
-        let rata_kredytu = kwota_kredytu * (((oprocentowanie / 12) * Math.pow((1 + oprocentowanie / 12),liczba_rat)) / (Math.pow((1 + (oprocentowanie / 12)), liczba_rat) - 1));
-        console.log("rata: " + rata_kredytu);
+        let stala_rata_kredytu = kwota_kredytu * (((oprocentowanie / 12) * Math.pow((1 + oprocentowanie / 12),liczba_rat)) / (Math.pow((1 + (oprocentowanie / 12)), liczba_rat) - 1));
+        console.log("rata: " + stala_rata_kredytu);
+        let malejaca_rata_kredytu = ((kwota_kredytu / liczba_rat) * (1 + (liczba_rat - i + 1) * oprocentowanie));
+        console.log("pierwsza malejąca rata kredytu: " + malejaca_rata_kredytu);
 
-        let odestki = (oprocentowanie / 12) * kwota_kredytu;
-        console.log("odsetki: " + odestki);
+        let odsetki = (oprocentowanie / 12) * kwota_kredytu;
+        console.log("odsetki: " + odsetki);
 
-        let kapitał = rata_kredytu - odestki;
-        console.log("kapitał: " + kapitał);
+        let kapital_stalej_raty = stala_rata_kredytu - odsetki;
+        console.log("kapitał stała rata: " + kapital_stalej_raty);
+        let kapital_malejacej_raty = malejaca_rata_kredytu - odsetki;
 
-        let kredytWartosc = kwota_kredytu;
-        var i = 0;
+        console.log("kapitał malejąca rata: " + kapital_malejacej_raty);
 
-        while(kredytWartosc > 0)
-        {
-          ++i;
-          let bilans = kredytWartosc * (((oprocentowanie / 12) * Math.pow((1 + oprocentowanie / 12),liczba_rat)) / (Math.pow((1 + (oprocentowanie / 12)), liczba_rat) - 1));
-          let rata = kredytWartosc - bilans;
-          let odsetkiWartosc = (oprocentowanie / 12) * kredytWartosc;
-          let kapitałWartosc = kredytWartosc - odestki;
-          console.log(i + ". " + "rata: " + rata + " kapitał: " + kapitałWartosc + " odsetki: " + odsetkiWartosc);
-        }
+        // while(kapitałWartosc > 0)
+        // {
+        //   i++;
+        //   let bilans = kredytWartosc * (((oprocentowanie / 12) * Math.pow((1 + oprocentowanie / 12),liczba_rat)) / (Math.pow((1 + (oprocentowanie / 12)), liczba_rat) - 1));
+        //   let rata = kredytWartosc - bilans;
+        //   let odsetkiWartosc = (oprocentowanie / 12) * kredytWartosc;
+        //   kapitałWartosc = kredytWartosc - odestki;
+        //   console.log(i + ". " + "rata: " + rata + " kapitał: " + kapitałWartosc + " odsetki: " + odsetkiWartosc);
+        // }
       }
     </script>
-    <?php 
+  </div>
+</x-app-layout>
+<?php 
     /**
      * AMORTIZATION CALCULATOR
      * @author PRANEETH NIDARSHAN
@@ -216,5 +239,3 @@
   
   
     ?>
-  </div>
-</x-app-layout>
